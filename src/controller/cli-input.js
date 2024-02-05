889963b8628ad009a391b46706436689157cb06c
Yury Staravoitau <yurystaravoitau@gmail.com>
2024-02-05 21:11:14 +0300

    feat: Add rm, mv and cp functionality

--- src/controller/cli-input.js
+++ src/controller/cli-input.js
@@ -1,7 +1,7 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { Readable, Writable, pipeline } from 'stream';
-import { createReadStream } from 'fs';
+import { createReadStream, createWriteStream } from 'fs';
 
 
 
@@ -45,21 +45,21 @@ export default class Cli_input_manager {
                 case 'cat':
                     this.readFile(params[0]);
                     break;
-                // case 'add':
-                //   createFile(params[0]);
-                //   break;
-                // case 'rn':
-                //   renameFile(params[0], params[1]);
-                //   break;
-                // case 'cp':
-                //   copyFile(params[0], params[1]);
-                //   break;
-                // case 'mv':
-                //   moveFile(params[0], params[1]);
-                //   break;
-                // case 'rm':
-                //   deleteFile(params[0]);
-                //   break;
+                case 'add':
+                    this.createFile(params[0]);
+                    break;
+                case 'rn':
+                    this.renameFile(params[0], params[1]);
+                    break;
+                case 'cp':
+                    this.copyFile(params[0], params[1]);
+                    break;
+                case 'mv':
+                    this.moveFile(params[0], params[1]);
+                    break;
+                case 'rm':
+                    this.deleteFile(params[0]);
+                    break;
                 // case 'os':
                 //   getOSInfo(params[0]);
                 //   break;
@@ -165,5 +165,89 @@ export default class Cli_input_manager {
             console.error('Error opening file:', error.message);
         }
     }
+
+    async createFile(fileName) {
+        try {
+            const fullPath = path.resolve(this.currentDirectory, fileName);
+            await fs.writeFile(fullPath, '');
+            console.log(`File ${fileName} created successfully in ${this.currentDirectory}`);
+        } catch (error) {
+            console.error('Error creating file:', error.message);
+        }
+    }
+
+    async renameFile(oldPath, newName) {
+        try {
+            const fullPath = path.resolve(this.currentDirectory, oldPath);
+            const newFullPath = path.resolve(this.currentDirectory, newName);
+            await fs.rename(fullPath, newFullPath);
+            console.log(`File ${oldPath} renamed to ${newName}`);
+        } catch (error) {
+            console.error('Error renaming file:', error.message);
+        }
+    }
+
+    async copyFile(sourcePath, destinationPath) {
+        try {
+            const sourceFullPath = path.resolve(this.currentDirectory, sourcePath);
+            const destinationFullPath = path.resolve(this.currentDirectory, destinationPath);
+
+            const readStream = createReadStream(sourceFullPath);
+            const writeStream = createWriteStream(destinationFullPath);
+
+            readStream.pipe(writeStream);
+
+            await new Promise((resolve, reject) => {
+                writeStream.on('finish', resolve);
+                writeStream.on('error', reject);
+            });
+
+            console.log(`File ${sourcePath} copied to ${destinationPath}`);
+        } catch (error) {
+            console.error('Error copying file:', error.message);
+        }
+    }
+
+    async moveFile(sourcePath, destinationPath) {
+        try {
+            const sourceFullPath = path.resolve(this.currentDirectory, sourcePath);
+            const destinationFullPath = path.resolve(this.currentDirectory, destinationPath);
+
+            const sourceStats = await fs.stat(sourceFullPath);
+
+            if (sourceStats.isFile()) {
+                const readStream = createReadStream(sourceFullPath);
+                const writeStream = createWriteStream(destinationFullPath);
+
+                readStream.pipe(writeStream);
+
+                await new Promise((resolve, reject) => {
+                    writeStream.on('finish', async () => {
+                        await fs.unlink(sourceFullPath);
+                        resolve();
+                    });
+                    writeStream.on('error', reject);
+                });
+
+                console.log(`File ${sourcePath} moved to ${destinationPath}`);
+            } else {
+                console.error(`Error moving file: ${sourcePath} is a directory. Use 'cp -r' for directory copy.`);
+            }
+        } catch (error) {
+            console.error('Error moving file:', error.message);
+        }
+    }
+
+
+    async deleteFile(filePath) {
+        try {
+            const fullPath = path.resolve(this.currentDirectory, filePath);
+            await fs.unlink(fullPath);
+            console.log(`File ${filePath} deleted successfully`);
+        } catch (error) {
+            console.error('Error deleting file:', error.message);
+        }
+    }
+
 
 }
